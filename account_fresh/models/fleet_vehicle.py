a1374b34b01ad7d3e597db4f40ad4245d0935f86
id10tttt <dgqcjx@gmail.com>
2018-03-22 17:23:52 +0800

    删除@api.constrains(company_id),这样不会生效，修改对重复车辆牌照的提醒

--- account_fresh/models/fleet_vehicle.py
+++ account_fresh/models/fleet_vehicle.py
@@ -8,7 +8,6 @@ class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
     _name = 'fleet.vehicle'
 
-    # _sql_constraints = [('license_plate_uniq', 'unique (license_plate)', "license_plate already exists !")]
     # company_id = fields.Many2one('res.company', 'Company', required=True)
 
 
@@ -17,13 +16,8 @@ class FleetVehicle(models.Model):
     def _validate_plate(self):
         res = self.env['fleet.vehicle'].sudo().search([('license_plate', '=', self.license_plate)])
         if len(res) > 1:
-            raise ValidationError(u'已经存在该车牌，不能创建相同的车牌!!!')
+            raise ValidationError(u'已经存在该车牌 [%s]，不能创建相同的车牌!!!' % self.license_plate)
         else:
             if not res.company_id:
                 raise ValidationError(u'必须选中公司哦!')
 
-    @api.constrains('company_id')
-    def _check_company(self):
-        print self.company_id
-        if not self.company_id:
-            raise ValidationError('You must choose company!')
