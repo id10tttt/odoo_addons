4cde0fe9503245eefa56fd238639ee3f7b89605c
id10tttt <dgqcjx@gmail.com>
2018-03-23 10:08:49 +0800

    不确定车队模块，为什么车辆型号等，可以重复建立，暂时做一个限制，不能重复

--- account_fresh/models/fleet_vehicle.py
+++ account_fresh/models/fleet_vehicle.py
@@ -14,10 +14,33 @@ class FleetVehicle(models.Model):
 
     @api.constrains('license_plate')
     def _validate_plate(self):
-        res = self.env['fleet.vehicle'].sudo().search([('license_plate', '=', self.license_plate)])
+        company = self.env['res.company']._company_default_get()
+        res = self.env['fleet.vehicle'].sudo().search([('license_plate', '=', self.license_plate),
+                                                       ('company_id', '=', company.id)])
         if len(res) > 1:
             raise ValidationError(u'已经存在该车牌 [%s]，不能创建相同的车牌!!!' % self.license_plate)
         else:
             if not res.company_id:
-                raise ValidationError(u'必须选中公司哦!')
+                raise ValidationError(u'必须选择公司!')
 
+class FleetVehicleModelBrand(models.Model):
+    _inherit = 'fleet.vehicle.model.brand'
+    _name = 'fleet.vehicle.model.brand'
+
+
+    @api.constrains('name')
+    def _check_name(self):
+        res = self.env['fleet.vehicle.model.brand'].sudo().search([('name', '=', self.name)])
+        if len(res) > 1:
+            raise ValidationError(u'已经存在该型号 [%s]，不能创建相同的型号!!!' % self.name)
+
+class FleetVehicleModel(models.Model):
+    _inherit = 'fleet.vehicle.model'
+    _name = 'fleet.vehicle.model'
+
+    @api.constrains('name')
+    def _check_name(self):
+        res = self.env['fleet.vehicle.model'].sudo().search([('name', '=', self.name),
+                                                             ('brand_id', '=', self.brand_id.id)])
+        if len(res) > 1:
+            raise ValidationError(u'已经存在该车辆型号 [%s]，不能创建相同的车辆型号!!!' % self.name)
