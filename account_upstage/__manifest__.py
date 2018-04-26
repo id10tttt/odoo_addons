17718902dd55e3fc2f6470f88374b2530be9a506
id10tttt <dgqcjx@gmail.com>
2018-04-26 14:33:44 +0800

    科目只显示末级

---
+++ account_upstage/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'account account last',
+    'description': 'account account last',
+    'author': '1di0t',
+    'depends': ['base',
+                'account',
+                'hr_expense',
+                'purchase'],
+    'application': True,
+    'data': [
+        'views/account_upstage.xml',
+    ],
+}
