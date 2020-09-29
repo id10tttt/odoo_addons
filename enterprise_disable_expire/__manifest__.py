0c838b3cdf0d2701b76c4dce940b74f33466ea3a
1di0t <dgqcjx@gmail.com>
2020-09-29 16:39:18 +0800

    disable enterprise expire check

---
+++ enterprise_disable_expire/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Enterprise Disable Expire",
+    "author": "1di0t",
+    "sequence": 1,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "summary": """
+        Enterprise Disable Expire
+        """,
+    "description": """
+        Enterprise Disable Expire
+        """,
+    "depends": [
+        'web_enterprise'
+    ],
+    "data": [
+        "views/asset.xml",
+    ],
+}
