72fcb71cb2b8c9848fc6f2d44643ac9bbac23737
1di0t <dgqcjx@gmail.com>
2020-10-30 14:39:14 +0800

    delete

--- enterprise_disable_expire/static/src/js/disable_expire.js
+++
@@ -1,9 +0,0 @@
-odoo.define('enterprise_disable_expire.ExpirationPanel', function (require) {
-    let HomeMenu = require('web_enterprise.HomeMenu');
-
-    let DisableExpireHomeMenu = HomeMenu.include({
-        _enterpriseExpirationCheck: function () {
-            return;
-        }
-    })
-})
