1b99ca71600edd229885bd1e98bca57dfeced809
1di0t <dgqcjx@gmail.com>
2020-12-10 16:43:25 +0800

    输入搜索字段

---
+++ odoo_search_fields/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo search field",
+    'description': """
+        Odoo search field
+        """,
+    'author': '1di0t',
+    'website': '',
+    'application': True,
+    'depends': ['base', 'web'],
+    'data': [
+    ],
+    'qweb': [
+        "static/src/xml/base.xml"
+    ]
+}
